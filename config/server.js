da605fc6d1762a1f2a3308d10ff4b7f676ba52f6
ahmadniaz <ahmadniazpk123@gmail.com>
2021-11-03 07:40:12 -0700

    resolved error in server.js

--- config/server.js
+++ config/server.js
@@ -1,10 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-<<<<<<< HEAD
   url:'https://strapi-backend-organic.herokuapp.com',
-=======
->>>>>>> 1c5e86b (ReUploading to git repo)
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '4a65c6638e6407fe514ef593c73d3d4c'),
